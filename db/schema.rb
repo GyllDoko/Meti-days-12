7260fe767210adc367b82fbc1d9e3d15d83d91b8
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2022-11-22 12:49:49 +0100

    Blog created successfully

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_102120) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_114602) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
